d6b1ca16a7aee7ad050802667f25b3c2284861cd
Jit <prithvirajpramanik@yahoo.co.in>
2016-01-25 17:47:33 +0530

    Add email,password response

--- api.py
+++ api.py
@@ -1,12 +1,27 @@
 from flask import Flask 
-from flask_restful  import Resources, Api
+from flask_restful  import Resource, Api, reqparse
 
 app = Flask(__name__)
 api=Api(app)
 
+
+
 class CreateUser(Resource):
 	def post(self):
-		return {'status':'success'}
+		try:
+			parser= reqparse.RequestParser()
+			parser.add_argument('email',type=str,help='Email address to create user')
+			parser.add_argument('password',type=str,help='Password to create user')
+			args= parser.parse_args()
+
+			_userEmail = args['email']
+			_userPassword = args['password']
+			
+			return {'Email':args['email'], 'Password':args['password']}	
+			#return {'status':'success'}
+		except Exception as e:
+			return {'error':str(e)}
+
 
 api.add_resource(CreateUser, '/CreateUser')
 
